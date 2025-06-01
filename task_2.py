29964615a88c0da9493b02c672705f8fe0fc9a83
Evemode <evemod3@gmail.com>
2025-06-01 12:10:07 +0200

    task_2 finished

---
+++ task_2.py
@@ -0,0 +1,11 @@
+import random
+
+def get_numbers_ticket(min_num:int, max_num:int, quantity:int) -> list[int]:
+    '''choosing random numbers according to given parameters'''
+    if 0 < min_num <= quantity <=max_num <=1000: #checks conditions
+        chosen_numbers = sorted(random.sample(range(min_num, max_num), quantity)) #choosing random unique numbers and sorting them
+        return chosen_numbers
+    return []
+
+lottery_numbers = get_numbers_ticket(1, 1000, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
