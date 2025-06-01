9519310264c074aee8cc07e0c95f4cb922fa674a
Evemode <evemod3@gmail.com>
2025-06-01 20:35:50 +0200

    improved version

--- task_2.py
+++ task_2.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min_num:int, max_num:int, quantity:int) -> list[int]:
     '''choosing random numbers according to given parameters'''
-    if 0 < min_num <= quantity <=max_num <=1000: #checks conditions
+    if 0 < min_num and max_num <=1000: #checks conditions
         chosen_numbers = sorted(random.sample(range(min_num, max_num), quantity)) #choosing random unique numbers and sorting them
         return chosen_numbers
     return []
